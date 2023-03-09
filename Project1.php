c504b0936f42baa9c2cdeb0244a8f6e86d5e7685
FadhilRifgiPratama <126134817+FadhilRifgiPratama@users.noreply.github.com>
2023-03-09 12:36:43 +0700

    Add files via upload

---
+++ Project1.php
@@ -0,0 +1,19 @@
+<?php
+
+        $name = "Fadhil";
+        $age = 20;
+        $address = "Padalarang";
+
+        echo "Name : ";
+        echo $name;
+        echo "\n";
+
+        echo "Age : ";
+        echo $age;
+        echo "\n";
+
+        echo "Address : ";
+        echo $address;
+        echo "\n";
+
+?>
